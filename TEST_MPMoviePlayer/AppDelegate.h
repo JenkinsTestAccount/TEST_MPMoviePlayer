7c4031d43607880a25d80babbec0f9292b0d7274
user <user@vzhuk_mac.scnsoft.com>
2012-07-04 12:53:51 +0300

    Initial Commit

---
+++ TEST_MPMoviePlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TEST_MPMoviePlayer
+//
+//  Created by user on 04.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
